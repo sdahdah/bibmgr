b3771a3dc05e9e255069b13144bec3e2986cda97
Steven Dahdah <sdahdah@users.noreply.github.com>
2020-09-19 22:35:38 -0400

    Add biblib MWE

--- bibmgr/bibmgr.py
+++ bibmgr/bibmgr.py
@@ -1,2 +1,20 @@
+import argparse
+import sys
+import biblib.bib
+
+
 def main():
-    print('hello from bibmgr')
+    # TODO This is copy-pasted from the biblib examples
+    arg_parser = argparse.ArgumentParser(description='Parse .bib database(s)'
+                                         ' and print basic fields as text')
+    arg_parser.add_argument('bib', nargs='+', help='.bib file(s) to process',
+                            type=open)
+    args = arg_parser.parse_args()
+
+    db = biblib.bib.Parser().parse(args.bib, log_fp=sys.stderr).get_entries()
+
+    breakpoint()
+
+    for ent in db.values():
+        print(ent.to_bib())
+        print()

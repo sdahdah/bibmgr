219507adb3a59baaa5d2ce6660464c8e8920609a
Steven Dahdah <sdahdah@users.noreply.github.com>
2021-01-06 22:59:45 -0500

    Add ability to link new file

--- bibmgr/bibmgr.py
+++ bibmgr/bibmgr.py
@@ -19,18 +19,18 @@ def main():
             xdg_config_home = home.joinpath('.config')
         else:
             xdg_config_home = pathlib.Path(xdg_config_home_raw)
-        default_cfg_path = xdg_config_home.joinpath('bibmgr/bibmgr.cfg')
+        default_conf_path = xdg_config_home.joinpath('bibmgr/bibmgr.conf')
     else:
         # If on Windows, must specify manually for now
-        default_cfg_path = ''
+        default_conf_path = ''
 
     parser = argparse.ArgumentParser(description='')
     subparsers = parser.add_subparsers()
 
     # Shared arguments
     parser.add_argument('-c', '--config', metavar='CONFIG', type=str,
-                        dest='cfg_path', default=default_cfg_path,
-                        help='path to default configuration file (*.cfg)')
+                        dest='conf_path', default=default_conf_path,
+                        help='path to default configuration file (*.conf)')
     parser.add_argument('-l', '--library', metavar='LIBRARY', type=str,
                         dest='lib', default=None,
                         help='Name of library to use')
@@ -42,32 +42,33 @@ def main():
     org_parser.set_defaults(func=org)
     # Link subcommand
     link_parser = subparsers.add_parser('link', help='')
-    link_parser.add_argument('key', metavar='KEY', type=str, help='')
     link_parser.add_argument('pdf', metavar='PDF', type=str, help='')
+    link_parser.add_argument('-k', '--key', metavar='KEY', type=str,
+                             default=None, help='')
     link_parser.set_defaults(func=link)
 
     # Parse arguments
     args = parser.parse_args()
 
-    # Load and parse config files
-    if pathlib.Path(args.cfg_path).exists():
-        cfg = configparser.ConfigParser()
-        cfg.read(args.cfg_path)
+    # Load and parse config file
+    if pathlib.Path(args.conf_path).exists():
+        conf = configparser.ConfigParser()
+        conf.read(args.conf_path)
     else:
-        raise FileNotFoundError(args.cfg_path)
+        raise FileNotFoundError(args.conf_path)
 
-    # Choose library if none is specified
+    # Choose default library if none is specified
     if args.lib is None:
-        for section in cfg.keys():
-            if 'path' in cfg[section].keys():
-                selected_lib = section
-                break
+        selected_lib = conf['config']['default_library']
     else:
         selected_lib = args.lib
 
-    lib = Library(cfg[selected_lib]['path'],
-                  cfg.getint('config', 'filename_len'),
-                  cfg.getint('config', 'key_len'))
+    # Create Library object
+    lib = Library(conf[selected_lib]['bibtex_file'],
+                  conf[selected_lib]['storage_path'],
+                  conf.getint('config', 'filename_length'),
+                  conf.getint('config', 'key_length'),
+                  conf.getint('config', 'wrap_width'))
 
     # Run subcommand
     args.func(lib, args)
@@ -76,10 +77,11 @@ def main():
 def echo(lib, args):
     lib.open_bib_db()
     for entry in lib.db.values():
-        print(entry.to_bib(), end='\n\n')
+        print(entry.to_bib(wrap_width=lib.wrap_width), end='\n\n')
 
 
 def org(lib, args):
+    # TODO DONT MOVE IF EVERYTHING IS IN ORDER
     lib.open_bib_db()
     # Create new group folders
     lib.create_missing_groups()
@@ -88,30 +90,39 @@ def org(lib, args):
     # Move files to correct groups
     lib.move_according_to_bib()
     # Update keys according to metadata
-    # lib.rekey_according_to_bib()  # TODO Re-enable
+    lib.rekey_according_to_bib()
     # Write new bib file
     lib.write_bib_file()
 
 
 def link(lib, args):
     lib.open_bib_db()
-    lib.link_file(args.pdf, args.key)
+    # Make sure desired PDF exists
+    # TODO Also check that it's a file not a directory
+    pdf_path = pathlib.Path(args.pdf)
+    if not pdf_path.exists():
+        raise FileNotFoundError(args.pdf)
+    # Link file with filename as default key
+    lib.link_file(pdf_path, pdf_path.stem if args.key is None else args.key)
     lib.write_bib_file()
 
 
 class Library:
 
-    def __init__(self, lib_path, filename_len, key_len):
-        self.lib_path = pathlib.Path(lib_path)
-        self.bib_path = self.lib_path.joinpath(f'{self.lib_path.stem}.bib')
-        self.bak_path = self.lib_path.joinpath(f'{self.lib_path.stem}.bib.bak')
-        self.filename_len = filename_len
-        self.key_len = key_len
+    def __init__(self, bibtex_file, storage_path, filename_length, key_length,
+                 wrap_width):
+        # Paths and settings
+        self.bibtex_file = pathlib.Path(bibtex_file)
+        self.bibtex_bak_file = pathlib.Path(bibtex_file + '.bak')
+        self.storage_path = pathlib.Path(storage_path)
+        self.filename_length = filename_length
+        self.key_length = key_length
+        self.wrap_width = wrap_width
         self.db = None
 
     def create_missing_groups(self):
         for entry in self.db.values():
-            group_path = self.lib_path.joinpath(entry['groups'])
+            group_path = self.storage_path.joinpath(entry['groups'])
             try:
                 group_path.mkdir()
             except FileExistsError:
@@ -124,89 +135,98 @@ class Library:
                 biblib.algo.parse_names(entry['author'])[0].last)
             year = _clean_string(entry['year'])
             title = _clean_string(entry['title'])
-            filename = (name + '_' + year + '_' + title)[:self.filename_len]
+            filename = (name + '_' + year + '_' + title)[:self.filename_length]
             if filename == '':
+                # TODO DO SOMETHING MORE GRACEFUL?
                 raise RuntimeError(f"New file name for key '{key}' is empty, "
                                    f"cannot rename.")
-            pdf_path = \
-                self.lib_path.joinpath(self.read_relative_path(entry['file']))
+            if 'file' not in entry.keys():
+                # TODO Maybe warn here?
+                continue
+            pdf_path = pathlib.Path(entry['file'])
             ext = ''.join(pdf_path.suffixes)
             new_path = pdf_path.parent.joinpath(filename + ext)
+            # Double check if path points to a file to avoid accidentally
+            # moving directory. `is_file()` is the most important check here.
+            # TODO Maybe encapsulate this
+            if (('file' not in entry.keys()) or (entry['file'] == '')
+                    or (not pdf_path.exists()) or (not pdf_path.is_file())):
+                # TODO Maybe warn here?
+                continue
             shutil.move(pdf_path, new_path)
-            entry['file'] = str(self.format_relative_path(new_path))
+            entry['file'] = str(new_path)
 
     def move_according_to_bib(self):
         for key, entry in zip(self.db.keys(), self.db.values()):
-            pdf_path = self.read_relative_path(entry['file'])
-            new_path = \
-                self.lib_path.joinpath(entry['groups']).joinpath(pdf_path.name)
+            if 'file' not in entry.keys():
+                # TODO Maybe warn here?
+                continue
+            pdf_path = pathlib.Path(entry['file'])
+            new_path = self.storage_path.joinpath(
+                entry['groups']).joinpath(pdf_path.name)
+            # Double check if path points to a file to avoid accidentally
+            # moving directory. `is_file()` is the most important check here.
+            # TODO Maybe encapsulate this
+            if (('file' not in entry.keys()) or (entry['file'] == '')
+                    or (not pdf_path.exists()) or (not pdf_path.is_file())):
+                # TODO Maybe warn here?
+                continue
             shutil.move(pdf_path, new_path)
-            entry['file'] = str(self.format_relative_path(new_path))
+            entry['file'] = str(new_path)
 
-    # def rekey_according_to_bib(self):
-    #     new_db = collections.OrderedDict()
-    #     for key, entry in zip(self.db.keys(), self.db.values()):
-    #         # Take last name of first author
-    #         name = _clean_string(
-    #             biblib.algo.parse_names(entry['author'])[0].last)
-    #         year = _clean_string(entry['year'])
-    #         title = _clean_string(entry['title'].split(' ')[0])
-    #         new_key = (name + '_' + year + '_' + title)[:self.key_len]
-    #         # TODO Fine-tune this behaviour
-    #         if new_key == '':
-    #             continue
-    #         # TODO Fine-tune this behaviour too
-    #         while new_key in new_db.keys():
-    #             new_key += '_dup'
-    #         entry.key = new_key  # TODO ???
-    #         new_db[new_key] = entry
-    #     self.db = new_db
+    def rekey_according_to_bib(self):
+        new_db = collections.OrderedDict()
+        for key, entry in zip(self.db.keys(), self.db.values()):
+            # Take last name of first author
+            name = _clean_string(
+                biblib.algo.parse_names(entry['author'])[0].last)
+            year = _clean_string(entry['year'])
+            title = _clean_string(entry['title'].split(' ')[0])
+            new_key = (name + '_' + year + '_' + title)[:self.key_length]
+            # If new key is empty, don't change it
+            if new_key == '':
+                # TODO Print warning
+                new_key = key
+            # If there's a duplicate, change the name
+            while new_key in new_db.keys():
+                # TODO Print warning
+                new_key += '_dup'
+            entry.key = new_key
+            new_db[new_key] = entry
+        self.db = new_db
 
-    def link_file(self, key, pdf):
-        # Make sure desired PDF exists
-        pdf_path = pathlib.Path(pdf)
-        if not pdf_path.exists():
-            raise FileNotFoundError(pdf)
-
-        self.db[key.lower()]['file'] = str(self.format_relative_path(pdf_path))
+    def link_file(self, pdf_path, key):
+        # def gen_db_with_blank_entry(self, key, file):
+        #     entry = (
+        #         f'@misc{{{key},\n'
+        #         f'  file = {{{file}}},\n'
+        #         f'}}'
+        #     )
+        if key.lower() in self.db:
+            self.db[key.lower()]['file'] = str(pdf_path.resolve())
+        else:
+            self.db[key.lower()] = biblib.bib.Entry(
+                [('file', str(pdf_path.resolve()))], key=key.lower(),
+                typ='misc')
 
     def open_bib_db(self):
-        """Opens and reads contents of BibTeX file.
-
-        Parameters
-        ----------
-        bib_path : pathlib.Path
-            Path of BibTeX file.
-        """
-        with open(self.bib_path, 'r') as bib:
+        """Opens and reads contents of BibTeX file."""
+        with open(self.bibtex_file, 'r') as bib:
             self.db = biblib.bib.Parser().parse(bib).get_entries()
 
     def write_bib_file(self):
         """Writes BibTeX dictionary to file.
         """
         # Delete .bak file if it exists
-        self.bak_path.unlink(missing_ok=True)
+        self.bibtex_bak_file.unlink(missing_ok=True)
         # Rename .bib file to .bib.bak
-        if self.bib_path.exists():
-            self.bib_path.rename(self.bak_path)
+        if self.bibtex_file.exists():
+            self.bibtex_file.rename(self.bibtex_bak_file)
         # Write new .bib file
-        with open(self.bib_path, 'a') as bib:
+        with open(self.bibtex_file, 'a') as bib:
             for entry in self.db.values():
-                bib.write(entry.to_bib())
+                bib.write(entry.to_bib(wrap_width=self.wrap_width))
                 bib.write('\n\n')
-
-    def format_relative_path(self, path):
-        # TODO Update to is_relative_to once 3.9 is a thing
-        try:
-            return path.resolve().relative_to(self.lib_path)
-        except ValueError:
-            return path.resolve()
-
-    def read_relative_path(self, path):
-        try:
-            return pathlib.Path(path).resolve(strict=True)
-        except FileNotFoundError:
-            return self.lib_path.joinpath(path)
 
 
 def _clean_string(s):
